975aaa9aebb771ca61ca9706c5dccc26271b97fd
farzeenkhantareen <farzeenkhan1199@gmail.com>
2025-09-12 21:30:00 +0500

    Initial commit

---
+++ app/src/main/java/com/example/assignment1/Login_Username_Password.kt
@@ -0,0 +1,30 @@
+package com.example.assignment1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class Login_Username_Password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.login_username_password)
+
+        var backArrow = findViewById<ImageView>(R.id.backArrow)
+        backArrow.setOnClickListener {
+            val intent = Intent(this@Login_Username_Password, Login_Page::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        var signUp = findViewById<TextView>(R.id.signUp)
+        signUp.setOnClickListener {
+            val intent = Intent(this@Login_Username_Password, Sign_up::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
